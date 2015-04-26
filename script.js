82fa4cd8f9778541f8f45f2a288c57bc871293a7
Chris Kim <christopherjkim@gmail.com>
2015-04-25 21:10:48 -0400

    save and load ceolstring to location.hash

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello");
+function isReady () {
+  console.log("I'm ready!");
+  return true;
+}
+
+function saveCeol (ceolString) {
+  console.log("saveCeol", ceolString);
+  location.hash = ceolString;
+}
+
+function loadCeol () {
+  var ceolString = location.hash.slice(1);
+  console.log("loadCeol", ceolString);
+  return ceolString;
+}
